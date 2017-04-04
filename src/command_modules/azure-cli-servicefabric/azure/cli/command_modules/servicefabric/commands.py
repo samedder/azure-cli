5f054dd3acd034c86448d4883d68dcc77414d7d5
Edward Samson <edsamw@gmail.com>
2017-04-03 19:00:15 -0700

    Adding new format

--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/commands.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/commands.py
@@ -8,5 +8,14 @@
 from azure.cli.core.commands import cli_command
 from azure.cli.command_modules.servicefabric._factory import cf_sf_client
 
-cli_command(__name__, 'sf application-health', 'azure.servicefabric#AzureServiceFabricClientAPIs.get_application_health', cf_sf_client)
-cli_command(__name__, 'sf cluster-connect', 'azure.cli.command_modules.servicefabric.custom#sf_update_connection_endpoint')
+# Application commands
+
+cli_command(__name__, 'sf application health', 'azure.servicefabric#AzureServiceFabricClientAPIs.get_application_health', cf_sf_client)
+
+# Node commands
+
+cli_command(__name__, 'sf node remove-state', 'azure.servicefabric#AzureServiceFabricClientAPIs.remove_node_state', cf_sf_client)
+
+# Cluster commands
+
+cli_command(__name__, 'sf cluster connect', 'azure.cli.command_modules.servicefabric.custom#sf_update_connection_endpoint')
