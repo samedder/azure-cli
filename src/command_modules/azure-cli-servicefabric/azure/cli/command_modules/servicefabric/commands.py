e11f97d547db948a653853e5134927b71089d5d7
Edward Samson <edsamw@gmail.com>
2017-04-03 17:41:19 -0700

    Adding new commands

--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/commands.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/commands.py
@@ -9,3 +9,4 @@ from azure.cli.core.commands import cli_command
 from azure.cli.command_modules.servicefabric._factory import cf_sf_client
 
 cli_command(__name__, 'sf application-health', 'azure.servicefabric#AzureServiceFabricClientAPIs.get_application_health', cf_sf_client)
+cli_command(__name__, 'sf cluster-connect', 'azure.cli.command_modules.servicefabric.custom#sf_update_connection_endpoint')
--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/custom.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/custom.py
@@ -4,7 +4,35 @@
 # --------------------------------------------------------------------------------------------
 
 import azure.cli.core.azlogging as azlogging
-
+from azure.cli.core._config import az_config, set_global_config_value
+from azure.cli.core._util import CLIError
 
 logger = azlogging.get_az_logger(__name__)
 
+
+def sf_update_connection_endpoint(endpoint):
+    """
+    Updates the current cluster connection endpoint to point to the specified
+    url.
+
+    :param endpoint: The new endpoint url of a Service Fabric cluster
+    :type endpoint: String
+    """
+    set_global_config_value('servicefabric', 'endpoint', endpoint)
+
+def sf_get_connection_endpoint():
+    endpoint_url = az_config.get('servicefabric', 'endpoint', fallback=None)
+    if endpoint_url is not None:
+        return endpoint_url
+    else:
+        raise CLIError('Service Fabric cluster endpoint not set, connect to a cluster first')
+
+# Store string inside az_config.set and az_config.get
+
+# Add client argument to custom command
+
+# SF client operation action ->
+# Create factory in _factory
+# Factory creates client instance
+# Uses config to pull down connection string
+# Returns client to perform API
