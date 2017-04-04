e60ff9d3af344613df8a3c3e5afe846e9ec6c897
Edward Samson <edsamw@gmail.com>
2017-04-03 19:05:32 -0700

    Adding help info

--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_help.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_help.py
@@ -6,3 +6,20 @@
 from azure.cli.core.help_files import helps
 
 # pylint: disable=line-too-long
+
+helps['sf'] = """
+     type: group
+     short-summary: Manage Service Fabric clusters and perform cluster operations
+"""
+helps['sf cluster'] = """
+    type: group
+    short-summary: Connect to and manage entire Service Fabric clusters
+"""
+helps['sf node'] = """
+    type: group
+    short-summary: Manage the nodes that form a Service Fabric cluster
+"""
+helps['sf application'] = """
+    type: group
+    short-summary: Manage the applications running in a Service Fabric cluster
+"""
