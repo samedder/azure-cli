7817319ce5e49fd8c5b19ebc2ff7b93995e2ce20
Edward Samson <edsamw@gmail.com>
2017-04-03 19:08:54 -0700

    FIxing help formatting

--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_help.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_help.py
@@ -7,19 +7,19 @@ from azure.cli.core.help_files import helps
 
 # pylint: disable=line-too-long
 
-helps['sf'] = """
+helps[''] = """
      type: group
      short-summary: Manage Service Fabric clusters and perform cluster operations
 """
-helps['sf cluster'] = """
+helps['cluster'] = """
     type: group
     short-summary: Connect to and manage entire Service Fabric clusters
 """
-helps['sf node'] = """
+helps['node'] = """
     type: group
     short-summary: Manage the nodes that form a Service Fabric cluster
 """
-helps['sf application'] = """
+helps['application'] = """
     type: group
     short-summary: Manage the applications running in a Service Fabric cluster
 """
