57fd7c8d880e275ee2b36c11787185765824c82e
Edward Samson <edsamw@gmail.com>
2017-04-03 17:04:43 -0700

    Adding client factory

--- src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_factory.py
+++ src/command_modules/azure-cli-servicefabric/azure/cli/command_modules/servicefabric/_factory.py
@@ -3,9 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-#pylint: disable=line-too-long
-
-from azure.cli.core.commands import cli_command
-from azure.cli.command_modules.servicefabric._factory import cf_sf_client
-
-cli_command(__name__, 'sf application-health', 'azure.servicefabric#AzureServiceFabricClientAPIs.get_application_health', cf_sf_client)
+def cf_sf_client(_):
+    from azure.servicefabric import AzureServiceFabricClientAPIs
+    return AzureServiceFabricClientAPIs(False,'http://localhost:19080') 
