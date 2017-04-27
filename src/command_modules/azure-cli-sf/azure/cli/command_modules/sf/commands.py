67ac4735e9e28963fa6a656d5fea2450c99ea4b0
Edward Samson <edsamw@gmail.com>
2017-04-27 05:39:20 -0700

    Fixing break due to API change in SDK

--- src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/commands.py
+++ src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/commands.py
@@ -24,7 +24,7 @@ cli_command(__name__, 'sf application upgrade', 'azure.cli.command_modules.sf.cu
 
 cli_command(__name__, 'sf service list', 'azure.servicefabric#ServiceFabricClientAPIs.get_service_info_list', cf_sf_client)
 cli_command(__name__, 'sf service manifest', 'azure.servicefabric#ServiceFabricClientAPIs.get_service_manifest', cf_sf_client)
-cli_command(__name__, 'sf service application-name', 'azure.servicefabric#ServiceFabricClientAPIs.get_application_name', cf_sf_client)
+cli_command(__name__, 'sf service application-name', 'azure.servicefabric#ServiceFabricClientAPIs.get_application_name_info', cf_sf_client)
 cli_command(__name__, 'sf service report-health', 'azure.servicefabric#ServiceFabricClientAPIs.report_service_health', cf_sf_client)
 cli_command(__name__, 'sf service create', 'azure.cli.command_modules.sf.custom#sf_create_service')
 cli_command(__name__, 'sf service description', 'azure.servicefabric#ServiceFabricClientAPIs.get_service_description', cf_sf_client)
