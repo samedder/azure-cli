f3805a2d7a760484a68216c0c656b58567b60a5b
Edward Samson <edsamw@gmail.com>
2017-04-27 14:10:25 -0700

    Adding resolve API

--- src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/commands.py
+++ src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/commands.py
@@ -32,6 +32,7 @@ cli_command(__name__, 'sf service description', 'azure.servicefabric#ServiceFabr
 cli_command(__name__, 'sf service update', 'azure.cli.command_modules.sf.custom#sf_update_service')
 cli_command(__name__, 'sf service health', 'azure.servicefabric#ServiceFabricClientAPIs.get_service_health', cf_sf_client)
 cli_command(__name__, 'sf service health-policy', 'azure.servicefabric#ServiceFabricClientAPIs.get_service_health_using_policy', cf_sf_client)
+cli_command(__name__, 'sf service resolve', 'azure.servicefabric#ServiceFabricClientAPIs.resolve_service', cf_sf_client)
 
 # Partition commands
 
